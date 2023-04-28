faa50ea25e5b9708dbe06e1bd6b45228b06433ec
root <root@Sty-PC.localdomain>
2023-04-27 22:49:25 -0300

    Final commit

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,95 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+interface Joke {
+  joke: string
+}
+export default defineComponent({
+  name: 'Chuck-Norris-Jokes',
+  setup() {
+    const name = ref<string>('')
+    const joke = ref<Joke>('')
+
+    const getJoke = async () => {
+      try {
+        const response = await fetch(`https://api.chucknorris.io/jokes/random?name=${name.value}`)
+        const data = await response.json()
+        joke.value = data.value
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    return { name, joke, getJoke }
+  }
+})
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="container">
+    <form class="form" @submit.prevent="getJoke">
+      <div id="button-wrapper">
+        <h1 class="title">Type a name to get a Chuck Norries Joke!</h1>
+        <input type="text" placeholder="Enter a name" v-model="name" />
+        <button class="" type="submit" :disabled="!name">Get Joke</button>
+      </div>
+    </form>
+    <div id="joke" v-if="joke">{{ joke }}</div>
+  </div>
 </template>
-
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+.container {
+  display: grid;
+  grid-template-columns: repeat(10, 1fr);
+  grid-template-rows: repeat(10, 1fr);
+  height: 100vh;
+  background: #a9b4c2;
+  border: 10px solid rgb(103, 67, 67);
+  place-items: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.form {
+  grid-column: 4 / 8;
+  grid-row: 4 / 8;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-
-nav {
-  width: 100%;
-  font-size: 12px;
+#button-wrapper {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+.title {
+  font-size: 1.5rem;
+  margin-bottom: 1rem;
   text-align: center;
-  margin-top: 2rem;
+}
+#joke {
+  grid-column: 3 / span 6;
+  grid-row: 5 / span 6;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+input,
+button {
+  height: 2rem;
+  padding: 0.5rem;
+  font-size: 1rem;
+  border: 1px solid #ccc;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+input {
+  flex: 1;
+  margin-right: 0.5rem;
+  border: 2px solid #cdcbef;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+button {
+  background-color: #8000ff;
+  color: #fff;
+  border: none;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  margin: 1em;
 }
 </style>
